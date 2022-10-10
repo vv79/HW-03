6b99d207468c79abdedd6da9d98a6bb0db7cfb3e
vv79 <volentic.vladimir@gmail.com>
2022-10-10 19:29:37 +0300

    ЗАДАНИЕ 8.5 (HW-03)

--- newsPaper/settings.py
+++ newsPaper/settings.py
@@ -265,3 +265,11 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://localhost:6379'
+    }
+}
